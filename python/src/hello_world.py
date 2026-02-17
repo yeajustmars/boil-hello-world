fb9ffed2c05992f39ba4a31846bd709e268b5e31
yeajustmars <yeajustmars@gmail.com>
2026-02-16 19:58:53 -0600

    test python, javascript

--- python/src/hello_world.py
+++ python/src/hello_world.py
@@ -1,8 +1,8 @@
 def print_welcome_message():
     # This variable is interpolated by
     # Boilermaker at build time, and its value is taken from the `boilermaker.toml` file.
-    interpolated_message = "{{{welcome_message}}}";
-    print("{interpolated_message}");
+    interpolated_message = "{{welcome_message}}";
+    print(f"{interpolated_message}");
 
 def main():
     print("Hello boiled Python!");
