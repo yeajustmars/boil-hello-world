3e4f2fa0e2d5d8d80bb36e7aff0769eb21238b9d
yeajustmars <yeajustmars@gmail.com>
2026-02-16 20:05:07 -0600

    test javascript

--- javascript/src/hello_world.js
+++ javascript/src/hello_world.js
@@ -2,8 +2,8 @@
   const print_welcome_message = () => {
       // This variable is interpolated by
       // Boilermaker at build time, and its value is taken from the `boilermaker.toml` file.
-      interpolated_message = "{{welcome_message}}";
-      console.log(`${interpolated_message}`);
+      interpolatedMessage = "{{welcome_message}}";
+      console.log(`${interpolatedMessage}`);
   }
 
   console.log("Hello boiled Javascript!");
